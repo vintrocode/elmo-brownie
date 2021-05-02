172dbb80d5a238a7348495cc3d4cd090e7f11c13
Vince <vincetrost@protonmail.com>
2021-05-01 21:47:37 -0700

    update deploy_brownie to reproduce unprotected tx error

--- elmo/scripts/deploy_brownie.py
+++ elmo/scripts/deploy_brownie.py
@@ -1,4 +1,11 @@
-from brownie import ERC20, L2DepositedERC20
+import os
+from brownie import ERC20, L2DepositedERC20, accounts, network
 
 def main():
-    ERC20.deploy(1337, 'ELMO', {'from': a.default})
+    
+    print(network.show_active())
+    network.gas_limit(10000000)
+
+    accounts.add(os.environ['METAMASK_SECRET_KEY'])
+    accounts.default = accounts[1]
+    ERC20.deploy(1337, 'ELMO', {'from': accounts.default})
